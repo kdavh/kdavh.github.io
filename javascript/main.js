c5607143d59e4e500aa69cdd44348dd9686910c3
kellen <kellenhart@gmail.com>
2015-01-01 23:06:13 -0800

    bring everything up to date, do some clean up

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,6 @@
 
 $(function() {
+  if ( $('#home').length ) document.location = document.location.origin + "/about/"
   if ( $('#projects').length ) $('#project-tab').addClass('active');
-  console.log($('#about').length)
-  if ( $('#about').length ) {
-    $('#about-tab').addClass('active')
-  }
+  if ( $('#about').length ) $('#about-tab').addClass('active');
 });
