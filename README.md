daf4080f6c22ddc37fd692b750c21673392e3ae9
Kellen Hart <kellenhart@gmail.com>
2018-02-18 19:08:37 -0800

    strip down site, simplify

--- README.md
+++ README.md
@@ -4,10 +4,4 @@ How to: https://help.github.com/articles/using-jekyll-with-pages/
 
 For local devl:
 
-`bundle install`
-
-generate `./_site` folder
-`$ jekyll build`
-
-start server accessible on localhost:4000
-`jekyll serve`
+`make serve`
--- javascript/main.js
+++
@@ -1,6 +0,0 @@
-
-$(function() {
-  if ( $('#home').length ) document.location = document.location.origin + "/about/"
-  if ( $('#projects').length ) $('#project-tab').addClass('active');
-  if ( $('#about').length ) $('#about-tab').addClass('active');
-});
